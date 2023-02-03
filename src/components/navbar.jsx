2a3d4ef7b7ee961dd5c9253779dfdbd0e1ed9179
Kleyton72 <jean_santos1@ucne.edu.do>
2023-02-02 19:32:34 -0500

    Setting up the navbar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import SearchBar from "./searchbar";
+import logo from "../assets/images/logo.png"
 
 const Navbar = () => {
-    return <></>
+    return (
+        <nav className="navbar">
+            <Link to={"/"} className="homelink">
+                <img src={logo}/>
+                <p>Minimalist <span>Reddit</span></p>
+            </Link>
+            <SearchBar/>
+            <button type="button" className="loginButton" value={"d"}>
+                Log In
+            </button>
+        </nav>
+    )
 }
 
 export default Navbar;
--- src/components/searchBar.jsx
+++ src/components/searchBar.jsx
@@ -1,3 +1,22 @@
+import React, { useRef } from "react"
+import { FaSearch } from "react-icons/fa";
+
 const SearchBar = () => {
-    return <></>
+
+    const searchInputRef = useRef();
+
+    return (
+        <div className="searchBar">
+            <input 
+                type="text" 
+                className="searchInput" 
+                ref={searchInputRef}
+                placeholder="Search"/>
+            <button type="button" className="searchButton">
+                <FaSearch/>
+            </button>
+        </div>
+    )
 }
+
+export default SearchBar;
